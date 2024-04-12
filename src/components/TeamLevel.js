eec6af49ea533ad20a5c8174cf7b8da0f77d60cd
Seth Miller <millerado@gmail.com>
2024-04-12 09:12:32 -0600

    pass state functions through props to playerbox

--- src/components/TeamLevel.js
+++ src/components/TeamLevel.js
@@ -1,13 +1,18 @@
 import PlayerBox from './PlayerBox';
 
-function TeamLevel({ team }) {
+function TeamLevel({ team, setPopupState, setPlayer }) {
   return (
     <div className='team-box'>
       <h2 className='team-name'>{team.name}</h2>
       <p className='team-description'>{team.description}</p>
       <div className='player-grid'>
         {team.players.map((player, index) => (
-          <PlayerBox key={index} player={player} />
+          <PlayerBox
+            key={index}
+            player={player}
+            setPopupState={setPopupState}
+            setPlayer={setPlayer}
+          />
         ))}
       </div>
     </div>
