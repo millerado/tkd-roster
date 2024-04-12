ae2acd1b3a1fbb471c87d15db1395fdba3050f84
Seth Miller <millerado@gmail.com>
2024-04-12 09:11:55 -0600

    import popup component and create states do deal with popup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Header from './components/Header';
 import TeamLevel from './components/TeamLevel';
+import PlayerPopup from './components/PlayerPopup';
 import allTeams from './teamData';
+import { useState } from 'react';
 
 function App() {
+  const [popupState, setPopupState] = useState(false);
+  const [player, setPlayer] = useState({});
   return (
     <div className='App'>
       <Header />
+      <PlayerPopup trigger={popupState} player={player} />
+      <h1>Team Kwik Discs</h1>
       {allTeams.map((team, index) => (
-        <TeamLevel key={index} team={team} />
+        <TeamLevel
+          key={index}
+          team={team}
+          setPopupState={setPopupState}
+          setPlayer={setPlayer}
+        />
       ))}
     </div>
   );
