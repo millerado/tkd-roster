25eb0135e1909ab8326e679a22d0fd3510c00e79
Seth Miller <millerado@gmail.com>
2024-04-28 13:27:56 -0600

    added classname

--- src/components/TeamLevel.js
+++ src/components/TeamLevel.js
@@ -2,7 +2,7 @@ import PlayerBox from './PlayerBox';
 
 function TeamLevel({ team, setPopupState, setPlayer }) {
   return (
-    <div className='team-box'>
+    <div className='team-box' id={team.name}>
       <h2 className='team-name'>{team.name}</h2>
       <p className='team-description'>{team.description}</p>
       <div className='player-grid'>
