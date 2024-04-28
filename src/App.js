f327dfc7a8bce79dbbf377a055a9b303e4bee89d
Seth Miller <millerado@gmail.com>
2024-04-28 13:31:07 -0600

    added team TKD description and buttons to scroll to team"

--- src/App.js
+++ src/App.js
@@ -17,6 +17,30 @@ function App() {
         setPopupState={setPopupState}
       />
       <h1>Team Kwik Discs</h1>
+      <p className='team-description'>
+        Our mission is to unite individuals through the shared passion for disc
+        golf, cultivating a culture of respect and relentless pursuit of
+        excellence. We strive to create an environment where every team member
+        is empowered to reach their full potential, both on and off the
+        course.Through unwavering sportsmanship and camaraderie, we aim to
+        elevate the standard of disc golf, embodying the principles of fair
+        play, integrity, and teamwork. As ambassadors of the sport, we commit to
+        fostering a community that values diversity, embraces challenges, and
+        celebrates the unique strengths each player brings to our team. Our
+        mission extends beyond the pursuit of victories; we aspire to inspire,
+        uplift, and leave a lasting impact on the disc golf community, promoting
+        a legacy of athleticism, character, and shared accomplishment.
+      </p>
+      <h2 className='team-name'>Meet the Teams</h2>
+      <div className='teams'>
+        {allTeams.map((team) => (
+          <h3>
+            <a className='team-button' href={`#${team.name}`}>
+              {team.name}
+            </a>
+          </h3>
+        ))}
+      </div>
       {allTeams.map((team, index) => (
         <TeamLevel
           key={index}
